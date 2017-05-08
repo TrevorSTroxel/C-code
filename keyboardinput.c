e9e13aed74a8df4d481180c2cee3db120f40f971
TrevorSTroxel <actroxel@gmail.com>
2017-05-08 13:43:05 -0700

    Add files via upload

---
+++ keyboardinput.c
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+#include <math.h>
+
+double main(int argc, const char * argv[])     /* program which introduces keyboard input */
+{
+float a, b, c, sum, sroot;
+
+printf("Enter three numbers to add\n");
+printf("Enter all three on the same line\n");
+scanf("%f%f%f",&a,&b,&c);
+
+sum = a + b + c;
+
+printf("Sum of entered numbers = %f\n",sum);
+
+sroot = sqrt(sum);
+
+printf("The square root of the sum = %f\n", sroot);
+
+return 0;
+
+//printf("Type in a number a\n");
+//scanf("%d", &a);
+//printf("Type in a number b\n");
+//scanf("%d", &b);
+//printf("Type in a number c\n");
+//scanf("%d", &c);
+//sum = a + b + c;
+//printf("The total is\n", sum);
+//scanf("%d", &sum);
+//scanf("%d%d%d",&a,&b,&c);
+//printf("The sum of the numbers were \n", sum);
+//sum = a + b + c;
+//scanf("%d%d%d", &sum);
+//printf("The sum of the numbers you typed in was %d\n", sum);
+}
+
